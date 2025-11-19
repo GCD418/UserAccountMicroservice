5bfe908636d4b949fc415a49e52fd7afe115df95
gabriel <patosquare418@gmail.com>
2025-11-19 19:52:14 -0400

    feat: program.cs configured

--- UserAccountMicroservice.Api/Program.cs
+++ UserAccountMicroservice.Api/Program.cs
@@ -1,4 +1,26 @@
+using UserAccountMicroservice.Application.Services;
+using UserAccountMicroservice.Domain.Ports;
+using UserAccountMicroservice.Infrastructure.Connection;
+using UserAccountMicroservice.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region DatabaseConnection
+
+var connectionString = builder.Configuration.GetConnectionString("PostgreSql");
+var connectionManager = DatabaseConnectionManager.GetInstance(connectionString!);
+builder.Services.AddSingleton(connectionManager);
+builder.Services.AddScoped<IDbConnectionFactory, PostgreSqlConnection>();
+
+#endregion
+
+#region UserAccount
+
+builder.Services.AddScoped<UserAccountService>();
+builder.Services.AddScoped<IUserAccountRepository, UserAccountRepository>();
+// builder.Services.AddScoped<IValidator<UserAccount>, UserAccountValidator>();
+
+#endregion
 
 // Add services to the container.
 
